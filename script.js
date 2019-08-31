93c6fafb578a649836f365bfdf10a46e9e847a40
Rafael Teixeira <rafael.amtx@gmail.com>
2019-08-30 23:10:00 -0300

    scoreboard and refresh-button

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function iniciarJogo(){
     for( i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('Game Over');
+            alert('Game Over' + '\nVocê fez ' + document.getElementById('pontos').textContent + ' pontos!!');
         }
     }
 
@@ -73,6 +73,8 @@ function iniciarJogo(){
     }else{
         food.x = Math.floor(Math.random() * 15 +1) * box;
         food.y = Math.floor(Math.random() * 15 +1) * box;
+        let valor = document.getElementById('pontos').textContent;
+        document.getElementById('pontos').innerHTML = parseInt(valor) + 1;
     }
 
     let newHead = {
